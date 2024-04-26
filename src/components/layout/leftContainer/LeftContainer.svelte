9c699eab017015ec82db6a7854f8cb1f732e193b
jackytruongpro <jacky-t@hotmail.fr>
2024-04-26 10:19:21 +0200

    Add some game pages

---
+++ src/components/layout/leftContainer/LeftContainer.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import avatar from '$lib/images/avatar_example.png';
+	import Button from '../../Button.svelte';
+	import InputText from '../../InputText.svelte';
+	let isInvitedWithLink: boolean = false;
+	import logo from '$lib/images/logo.svg';
+</script>
+
+<div class="left-container">
+	<img src={logo} alt="logo" />
+	<h2>Manche : 1 / 10</h2>
+	<div class="players-list">
+		<h2>Joueurs: 12</h2>
+		<ul>
+			<li>Element</li>
+			<li>Element</li>
+			<li>Element</li>
+			<li>Element</li>
+			<li>Element</li>
+			<li>Element</li>
+			<li>Element</li>
+			<li>Element</li>
+			<li>Element</li>
+		</ul>
+	</div>
+</div>
+
+<style lang="scss">
+	.left-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 30%;
+		padding: 20px;
+		margin-top: -30vw;
+		position: relative;
+		img {
+			margin-bottom: 1.7vw;
+			width: 15vw;
+		}
+		h2 {
+			font-size: 1.7vw;
+			color: white;
+			text-align: center;
+		}
+		.players-list {
+			width: 75%;
+			background-color: #255b9d;
+			border-radius: 10px;
+			margin-top: 30vw;
+			position: absolute;
+
+			h2 {
+				margin-top: 20px;
+			}
+
+			ul {
+				margin-top: 1vw;
+				background-color: #094d92;
+				border-radius: 10px;
+			}
+		}
+	}
+</style>
--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -18,4 +18,47 @@
 			console.log('disconnected');
 		});
 	});
+
+	// Main layouts
+	import LeftContainer from '../../components/layout/leftContainer/LeftContainer.svelte';
+	import ModeContainer from '../../components/layout/modeContainer/ModeContainer.svelte';
+	import Code from '../../components/layout/code/Code.svelte';
+	import Header from '../../components/layout/header/Header.svelte';
+	import Footer from '../../components/layout/footer/Footer.svelte';
+
+	let isDisplayHomePage: boolean = true;
+
+	let isDisplayLeftContainer: boolean = true;
+	let isDisplayRightModeContainer: boolean = true;
 </script>
+
+<div class="container">
+	<div class="home-content">
+		{#if isDisplayLeftContainer}
+			<LeftContainer />
+		{/if}
+		{#if isDisplayRightModeContainer}
+			<ModeContainer />
+		{/if}
+	</div>
+</div>
+
+<style lang="scss">
+	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: 77vw;
+		margin: 8vw auto 0 auto;
+
+		.home-content {
+			display: flex;
+			flex-direction: row;
+			justify-content: center;
+			width: 100%;
+			height: 55vh;
+			margin-top: 5vh;
+		}
+	}
+</style>
